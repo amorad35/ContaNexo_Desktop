b8b14a68e7e5499133616b2464a2513ef08cfdb8
amorad35 <amorad@uteq.edu.ec>
2026-09-04 03:09:33 -0500

    agregar clase de conexión a base de datos
    Se agrega la clase ConexionBD en ContaNexo.Data para centralizar la creación de conexiones SQL Server mediante una cadena de conexión recibida externamente.

---
+++ src/ContaNexo.Data/Connections/ConexionBD.cs
@@ -0,0 +1,18 @@
+using Microsoft.Data.SqlClient;
+
+namespace ContaNexo.Data.Connections;
+
+public class ConexionBD
+{
+    private readonly string _cadenaConexion;
+
+    public ConexionBD(string cadenaConexion)
+    {
+        _cadenaConexion = cadenaConexion;
+    }
+
+    public SqlConnection CrearConexion()
+    {
+        return new SqlConnection(_cadenaConexion);
+    }
+}
