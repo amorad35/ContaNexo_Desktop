30acb6046c847bbe282a59a37547356d9176ce38
amorad35 <amorad@uteq.edu.ec>
2026-09-05 10:52:53 -0500

    Crear shell visual inicial de ContaNexo

---
+++ src/ContaNexo.Desktop/Views/InicioView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ContaNexo.Desktop.Views
+{
+    public partial class InicioView : UserControl
+    {
+        public InicioView()
+        {
+            InitializeComponent();
+        }
+    }
+}
